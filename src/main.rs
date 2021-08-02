f3f71f5a1d66b1abdc6fdd7bf4ed0a84ed4bdde9
Marc Espín Sanz <mespinsanz@gmail.com>
2021-08-02 22:03:58 +0200

    :building_construction: Improved public-key system and signing (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use chrono::prelude::*;
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
-use rand::rngs::OsRng;
-use rsa::pkcs1::ToRsaPublicKey;
-use rsa::{RsaPrivateKey, RsaPublicKey};
+use openssl::hash::MessageDigest;
+use openssl::pkey::{PKey, Private};
+use openssl::rsa::Rsa;
+use openssl::sign::{Signer, Verifier};
 use std::collections::hash_map;
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -42,6 +43,72 @@ impl BlockHash {
     }
 }
 
+struct BlockBuilder {
+    pub hash: Option<BlockHash>,
+    pub previous_hash: Option<BlockHash>,
+    pub timestamp: Option<DateTime<Utc>>,
+    pub payload: Option<String>,
+    pub address: Option<String>,
+    pub signature: Option<String>,
+}
+
+impl BlockBuilder {
+    pub fn new() -> Self {
+        Self {
+            hash: None,
+            previous_hash: None,
+            timestamp: None,
+            payload: None,
+            address: None,
+            signature: None,
+        }
+    }
+
+    pub fn payload(&mut self, payload: &str) -> &mut Self {
+        self.payload = Some(payload.to_string());
+        self
+    }
+
+    pub fn previous_hash(&mut self, previous_hash: &BlockHash) -> &mut Self {
+        self.previous_hash = Some(previous_hash.clone());
+        self
+    }
+
+    pub fn timestamp(&mut self, timestamp: DateTime<Utc>) -> &mut Self {
+        self.timestamp = Some(timestamp);
+        self
+    }
+
+    pub fn address(&mut self, address: &str) -> &mut Self {
+        self.address = Some(address.to_string());
+        self
+    }
+
+    pub fn sign_with(&mut self, acc: &SignedInfo) -> &mut Self {
+        // Terribly ugly, I know
+        let data = format!(
+            "{}{}{}{:?}",
+            self.address.as_ref().unwrap(),
+            self.timestamp.as_ref().unwrap(),
+            self.payload.as_ref().unwrap(),
+            self.previous_hash
+        );
+
+        self.signature = Some(acc.sign_data(data));
+        self
+    }
+
+    pub fn build(&self) -> Block {
+        Block::new(
+            &self.payload.as_ref().unwrap(),
+            self.timestamp.unwrap(),
+            &self.previous_hash,
+            &self.address.as_ref().unwrap(),
+            &self.signature.as_ref().unwrap(),
+        )
+    }
+}
+
 #[derive(Clone, Debug)]
 struct Block {
     pub hash: BlockHash,
@@ -49,6 +116,7 @@ struct Block {
     pub timestamp: String,
     pub payload: String,
     pub address: String,
+    pub signature: String,
 }
 
 impl Block {
@@ -57,10 +125,12 @@ impl Block {
         timestamp: DateTime<Utc>,
         previous_hash: &Option<BlockHash>,
         address: &str,
+        signature: &str,
     ) -> Self {
         let timestamp = timestamp.to_string();
         let payload = payload.to_string();
         let address = address.to_string();
+        let signature = signature.to_string();
         let previous_hash = previous_hash.clone();
         Self {
             hash: BlockHash::new(
@@ -73,6 +143,7 @@ impl Block {
             payload,
             previous_hash,
             address,
+            signature,
         }
     }
 }
@@ -120,53 +191,71 @@ impl Blockchain {
     }
 }
 
-struct Address {
-    pub public_address: String,
+struct SignedInfo {
+    pub keypair: PKey<Private>,
 }
 
-impl Address {
+impl SignedInfo {
+    pub fn new() -> Self {
+        let keypair = Rsa::generate(515).unwrap();
+        let keypair = PKey::from_rsa(keypair).unwrap();
+
+        Self { keypair }
+    }
+
+    pub fn sign_data(&self, data: String) -> String {
+        let data = data.as_bytes();
+
+        let mut signer = Signer::new(MessageDigest::sha256(), &self.keypair).unwrap();
+        signer.update(data).unwrap();
+        let signature = signer.sign_to_vec().unwrap();
+
+        signature
+            .iter()
+            .map(|n| n.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    }
     /*
-     * Just for testing
-     */
-    pub fn random() -> Self {
-        let mut rng = OsRng;
-        let bits = 2048;
-        let private_key = RsaPrivateKey::new(&mut rng, bits).unwrap();
-        let public_key = RsaPublicKey::from(&private_key);
+    pub fn verify_signature(&self, signature: Vec<u8>) -> bool {
+        let mut verifier = Verifier::new(MessageDigest::sha256(), &self.keypair).unwrap();
+        verifier.update(&self.data.as_bytes()).unwrap();
+        verifier.verify(&signature).is_ok()
+    }
+    */
+    pub fn get_public(&self) -> String {
+        let public_key = self.keypair.public_key_to_pem().unwrap();
+        let public_key = String::from_utf8(public_key).unwrap();
         let mut hasher = Sha1::new();
-        hasher.input_str(&public_key.to_pkcs1_pem().unwrap());
-        let public_address = hasher.result_str();
-        Self { public_address }
+        hasher.input_str(&public_key);
+        return hasher.result_str();
     }
 }
 
 fn main() {
     let mut blockchain = Blockchain::new();
 
-    println!("Creating address...");
+    let account = SignedInfo::new();
+    let public_address = account.get_public();
 
-    let address_1 = Address::random();
+    blockchain.add_block(
+        BlockBuilder::new()
+            .payload("Block 1")
+            .timestamp(Utc::now())
+            .address(&public_address)
+            .sign_with(&account)
+            .build(),
+    );
 
-    println!("Address: {} \n", address_1.public_address);
-
-    blockchain.add_block(Block::new(
-        "Block 1",
-        Utc::now(),
-        &None,
-        &address_1.public_address,
-    ));
-    blockchain.add_block(Block::new(
-        "Block 2",
-        Utc::now(),
-        &blockchain.last_block_hash,
-        &address_1.public_address,
-    ));
-    blockchain.add_block(Block::new(
-        "Block 3",
-        Utc::now(),
-        &blockchain.last_block_hash,
-        &address_1.public_address,
-    ));
+    blockchain.add_block(
+        BlockBuilder::new()
+            .payload("Block 2")
+            .timestamp(Utc::now())
+            .previous_hash(&blockchain.peek().unwrap().hash)
+            .address(&public_address)
+            .sign_with(&account)
+            .build(),
+    );
 
     for (block_hash, block) in blockchain.iter() {
         let hash = &block_hash.hash;
